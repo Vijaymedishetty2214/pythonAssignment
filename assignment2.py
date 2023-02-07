328c2d13fadc70ff3310e6d6283f5aa217b5fa3d
VJ Medishetty <vijaymedishetty652@gmail.com>
2023-02-07 19:31:57 +0000

    Initial commit

---
+++ assignment2.py
@@ -0,0 +1,72 @@
+# what is string?
+"It represents the text format with single quotes or double quotes"
+x = "I love my country"
+
+#what is variable?
+"it is used to store data in the variable which means"
+x = """i love my country"here" x is a variable and I love my country is stored in variable x"""
+
+#what are parameters?
+"the information or data which is store inside the box is defined as parameters"
+
+#what is an integer
+"an integer is a value without any decimals"
+a = 12345678
+
+#string types
+#slicing string
+
+b = "Learning, Python"
+print(b[5:])
+print(b[8:])
+print(b[:6])
+print(b[3:12])
+print(b[12:5])
+
+print(b[1:16])
+
+#negative index
+print(b[-14:-5])
+print(b[-1:-12])
+
+#upper case
+print(b.upper())
+print(b.lower())
+print(b.replace("Learn", "driv"))
+print(b.split())
+
+#concatenate string
+a = "learning"
+b = "python"
+c = a + b
+print(c)
+
+c = a + " " + b
+print(c)
+
+#string format
+
+size = 0
+quantity = 6
+price = 6.6
+order = "I want size {} of {} tomatos for the price of{}"
+print(order.format(size, quantity, price ))
+
+#escape characters
+txt = "I would like to watch \''Bahubali' movie tonight"
+print(txt)
+
+txt = "I would like to watch \\'Bahubali' movie tonight"
+print(txt)
+
+txt = "I would like to watch \n'Bahubali' movie tonight"
+print(txt)
+
+txt= "I would like to watch \r'Bahubali' movie tonight"
+print(txt)
+
+txt = "I would like to watch \t'Bahubali' movie tonight"
+print(txt)
+
+
+
---
+++ pythonassignment.py
@@ -0,0 +1,53 @@
+print("hello" + " " + "Angela")
+
+
+
+
+
+
+#Here is my first assignment, i will be learning the python from basics to core
+
+"""
+x,y,z are the variables
+ and also figured what type of variables it is
+ """
+
+x = 9
+y = 12.9
+z = -9j
+
+print(x)
+print(y)
+print(z)
+print(type(z))
+print(type(x))
+print(type(y))
+
+#convert int to folat type
+a = float(x)
+print(a)
+print(type(a))
+
+#convert float to int
+b = int(y)
+print(b)
+print(type(b))
+
+#convert complax to float
+c = complex(y)
+print(c)
+
+
+x = 98
+y = 99
+z = (x+y)
+print(z)
+
+
+fruits = {"apple", "banana", "cherry"}
+x, y, z = fruits
+print(x)
+print(x,y,z)
+
+b = "Learning, Python"
+print(b[5:15])
