21328c783443143c2bdd48ef487cd4c5df6e3222
vjmedishetty <vijaymedishetty652@gmail.com>
2023-02-08 23:12:08 +0000

    new file

---
+++ booleanAssignment.py
@@ -0,0 +1,20 @@
+#boolean
+#boolen will help us identifying whether the statement is true or false.
+
+a = 33
+b = 25
+if b > a:
+    print('b is greater than a')
+else:
+    print('b is not greater than a')
+
+#arithematic operators
+x = 45
+y = 10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+#x//y helps to find out the nearest whole number
+print(x//y)
